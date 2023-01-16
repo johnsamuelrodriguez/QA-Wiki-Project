1f69b7e4867b98ed1e0c112d4947b0f73d90d1ae
John Samuel S. Rodriguez <johnsamuelrodriguez@gmail.com>
2023-01-16 21:45:56 +0800

    wave function collapse

---
+++ data.js
@@ -0,0 +1,8 @@
+var items = 
+    [{
+        heading: 'heading sample',
+        mainContent: 'main-content-sample',
+        subContent: 'sub-content sample',
+        link: 'www.com.example',
+        images: []
+    }];
