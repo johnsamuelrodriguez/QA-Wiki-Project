43d1018af07d48866edb5d3499213d49bc20771c
John Samuel S. Rodriguez <johnsamuelrodriguez@gmail.com>
2023-01-23 15:15:15 +0800

    refactor

--- data/data.js
+++ data/data.js
@@ -1,8 +1,218 @@
-var items = 
-    [{
-        heading: 'heading sample',
-        mainContent: 'main-content-sample',
-        subContent: 'sub-content sample',
+var items =     [
+
+    {
+        heading: 
+        `Create a bug item using Azure the
+        new work items button`,
+        mainContent: " \
+        Boards   >>   Work Items \
+           → \
+        New Work Items button \
+         → \
+        bug",
+        subContent: `
+        go to a project that
+        you are assigned to
+
+        to create a bug item 
+        go to boards
+
+        then work item  
+       click the \
+         + New Work Items   button  
+       then select select bug 
+        `,
         link: 'youtube',
         images: ['gengar']
-    }];
+    },
+
+    {
+        heading: 
+        `Create a bug item using Azure
+        sprints taskboard `,
+        mainContent: " Boards \
+           >>   \
+        Sprints \
+           >>   \
+        Taskboard \
+           >>      " + `
+        scroll new column → \
+        click plus icon → click bug → \
+        fill up details`,
+
+        subContent: `
+        go to a project
+        that you are assigned to
+        at the left sidebar
+        go to boards
+        under the boards
+        you will find sprints
+
+        select Sprints
+        select Taskboard
+        you can see columns
+        user stories, new,  
+        active, resolved,
+        rejected, invalid,
+        and, closed  
+
+        scroll down to the new column
+        and click the plus icon
+        then click bug
+        fill it up
+        and you are done
+        `,
+        link: 'youtube',
+        images: ['gengar']
+    },
+
+
+    {
+        heading: 
+        `Create a bug item using Azure 
+        Backlog tab `,
+        mainContent: `\
+        Boards \
+                         >> \
+                        Sprints \
+                         >> \
+                        Backlog Tab \
+                         >> 
+                        hover on an item \
+                        until plus icon is seen →
+                        click bug →
+                        fill up details
+        `,
+
+        subContent: `
+        go to a project
+        that you are assigned to
+        at the left sidebar
+        go to boards
+        under the boards
+        you will find sprints
+
+        select Sprints
+        select Backlog tab
+        
+        hover to a user story
+        at the left side of it 
+        a plus icon can be found 
+
+        click the plus icon
+        and click bug  
+        and fill it up
+        `,
+        link: 'youtube',
+        images: ['gengar']
+    },
+
+
+    {
+        heading: 
+        `BUG REPORT REQUIREMENTS `,
+        mainContent: `\
+        Title, most updated Iteration, Reproduction steps, 
+        pre-condition (optional)  Expected results, actual results,
+        ,test evidences. Priority and Severity. 
+        and Environment
+        `,
+
+        subContent: `
+        The requirements of a bug report are:
+        TITLE
+        the title of the bug report
+        the bug item number can be added here 
+        ITERATION  
+        the iteration must be set 
+        to the most updated one 
+
+
+        REPRODUCTION STEPS
+        reproduction steps  are also  called repro steps 
+        are numerical list of procedure that are needed  to reproduce the bug 
+        it is composed of optional Pre-condition, 
+        expected results, actual results 
+
+        Pre-conditions are optional 
+        and it contains the initial condition 
+        if there is no need for the pre-condition 
+        there is no need to add it. 
+
+        Meanwhile, Expected results are 
+        descriptions or visuals that might help 
+        to set the criteria of the requirements.
+
+        Lastly, Actual results contains the 
+        descriptions and visuals of what state 
+        the application currently have.
+        
+        
+        
+        
+
+        TEST EVIDENCE
+        these are attachments that are included 
+        these are more likely to be images
+        you can add them by clicking 
+        the file attachment icon shaped 
+        as a paper clip at the upper right 
+        of the main section. 
+        that is where you can upload images 
+        and videos for reference. 
+
+        PRIORITY
+        priority number for the item
+        
+
+        SEVERINITY
+        severinity is divided into 4 ranks
+        1 for critical, 2 for high, 3 for medium, 
+        and 4 for low
+
+        
+        critical bug items 
+        are very detremental for the 
+        actual system 
+
+        high bug items
+        are issues but not necessary 
+        will stop you from doing 
+        what the application is meant to do
+
+        meduim bug items 
+        are bugs that aren't that severe
+        and not part of core functionality of application 
+
+        low bug items 
+        are issues that mostly in front end 
+        such as spelling fixes and translations
+
+        
+        ENVIRONMENT
+        found under system info
+        it tells what environment 
+        what you are testing on
+        `,
+        link: 'youtube',
+        images: ['gengar']
+    },
+
+
+
+    {
+        heading: 'Ask for feedback',
+        mainContent: 'ask the project manager and devs if something occurs ',
+        subContent: 
+        `
+        feedback and suggestions 
+        are essential
+        for bug reports.
+        `,
+
+
+        link: 'youtube',
+        images: ['gengar']
+    }
+    
+];
